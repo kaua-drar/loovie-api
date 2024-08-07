f4b45e74ec01549a2be06a800e3b9df8a8be1f7e
kaua-drar <kauadrar@gmail.com>
2024-08-07 00:25:04 -0300

    feat: register JwtModule in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,19 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/user/users.module';
 import { SecurityModule } from 'src/security/security.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [UsersModule, SecurityModule],
+  imports: [
+    UsersModule,
+    SecurityModule,
+    JwtModule.register({
+      global: true,
+      secret: 'secret',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
 })
 export class AuthModule {}
