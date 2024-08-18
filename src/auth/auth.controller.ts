4fdcd234563a44fe8b9401bb2faa0430fb7f43e5
Kauã Drar <78101033+kaua-drar@users.noreply.github.com>
2024-08-18 18:09:49 -0300

    feat: create me endpoint (#4)
    * feat: create AuthRequest interface
    
    * refactor: moved @Public decorator from entire controller to the specific sign_up and login endpoints
    
    * feat: create 'me' endoint to get current user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, HttpCode, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Req,
+  Res,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpBodyDto } from './dtos/sign-up-body.dto';
 import { UserSerializer } from 'src/user/user.serializer';
@@ -6,12 +16,13 @@ import { LoginDto } from './dtos/login.dto';
 import { instanceToPlain } from 'class-transformer';
 import { Response } from 'express';
 import { Public } from './decorators/auth.public.decorator';
+import { AuthRequest } from './auth.request';
 
-@Public()
 @Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @HttpCode(201)
   @Post('sign_up')
   async signUp(@Body() body: SignUpBodyDto, @Res() res: Response) {
@@ -36,7 +47,7 @@ export class AuthController {
     return res.set('Access-Token', token).json(data);
   }
 
-  @HttpCode(200)
+  @Public()
   @Post('login')
   async login(@Body() body: LoginDto, @Res() res: Response) {
     const { email, username, password } = body;
@@ -50,5 +61,13 @@ export class AuthController {
     const data = instanceToPlain(new UserSerializer(user));
 
     return res.set('Access-Token', token).json(data);
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('me')
+  async me(@Req() req: AuthRequest) {
+    const { user } = req;
+
+    return new UserSerializer(user);
   }
 }
